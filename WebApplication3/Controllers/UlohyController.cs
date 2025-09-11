080684431714d8820b76fb3c93f2f3755e3dfead
DudraDamian <DudraDamian@401B-PC01>
2025-09-11 13:52:06 +0200

    ulohy

--- WebApplication3/Controllers/UlohyController.cs
+++ WebApplication3/Controllers/UlohyController.cs
@@ -1,6 +1,57 @@
-﻿namespace WebApplication3.Controllers
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+namespace WebApplication3.Controllers
 {
-    public class UlohyController
+    public class UlohyController : Controller
     {
+        public IActionResult uloha1()
+        {
+            return View();
+        }
+        public IActionResult uloha2()
+        {
+            return View();
+        }
+        public IActionResult uloha3()
+        {
+            return View();
+        }
+        public IActionResult uloha4()
+        {
+            var userList = new List<UserInfo>()
+            {
+                new UserInfo
+                {
+                    Name="Test",
+                    Surname="Test",
+                    Email="test@gmail.com",
+                },
+                new UserInfo
+                {
+                    Name = "Jozef",
+                    Surname="Mrkvička",
+                    Email = "jozef@gmail.com",
+                } };
+            return View(userList);
+        }
+        public IActionResult uloha5()
+        {
+            var userList = new List<UserInfo>()
+            {
+                new UserInfo
+                {
+                    Name="Test",
+                    Surname="Test",
+                    Email="test@gmail.com",
+                },
+                new UserInfo
+                {
+                    Name = "Jozef",
+                    Surname="Mrkvička",
+                    Email = "jozef@gmail.com",
+                } };
+            return View(userList);
+        }
     }
 }
---
+++ WebApplication3/Models/UserInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication3.Models
+{
+    public class UserInfo
+    {
+        public string Name { get; set; } = "";
+        public string Surname { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+    }
+}
